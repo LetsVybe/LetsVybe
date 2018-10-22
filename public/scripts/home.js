8cde76ddf3b53af3f1923415ca57f5114f8de8a0
Kathrine Sweebe <kathrines@kathrines-macbookpro.roam.corp.google.com>
2018-10-22 12:27:39 -0700

    Committer: Kathrine Sweebe <kathrines@kathrines-macbookpro.roam.corp.google.com>
     On branch k1
     Your branch is up to date with 'origin/k1'.
    
     Changes to be committed:
    	modified:   .DS_Store
    	modified:   .firebaserc
    	new file:   .gitignore
    	modified:   .idea/workspace.xml
    	deleted:    firebase-debug.log
    	modified:   public/home.html
    	modified:   public/index.html
    	modified:   public/scripts/home.js
    	new file:   public/scripts/login.js
    
     Purpose:
            Adding login functionality
    	User can now login with a LetsVybe account, create a LetsVybe account, and login with Google
    	Other functions listed are not yet tested to be in working condition.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -34,64 +34,81 @@ window.onload = function(){
 
 }
 
+function set_challenge_vyber_info(uid){
+    let challenge_vyber_div = document.createElement('div');
+    challenge_vyber_div.setAttribute('class', 'challenge-vyber');
+    let challenge_vyber_img = document.createElement('img');
+    challenge_vyber_img.setAttribute('class', 'challenge-vyber-img');
+    let challenge_vyber_name = document.createElement('p');
+    challenge_vyber_name.setAttribute('class', 'challenge-vyber-name')
+    challenge_vyber_div.appendChild(challenge_vyber_img);
+    challenge_vyber_div.appendChild(challenge_vyber_name);
+    users_ref.doc(uid).get()
+        .then(result => {
+            challenge_vyber_img.src = result.data().photoURL;
+            challenge_vyber_name.innerHTML = result.data().name;
+        }).catch(error => {
+        console.log(error.message);
+    })
+    return challenge_vyber_div
+}
 
-function create_feed(question_obj, post_answer){
+function feed(question, answer){
+    console.log(question.data());
 
-    console.log(question_obj.data());
+    // Create a div to house the challenge
+    let challenge_div = document.createElement('div');
+    challenge_div.setAttribute('class', 'challenge');
 
-    let question = question_obj.data();
-    let post_id = question_obj.id;
-
+    // Create a div to house the image and name of the vyber whose challenge it is
+    let challenge_vyber_div = set_challenge_vyber_info(question.uid);
+    challenge_div.appendChild(challenge_vyber_div);
 
     // Create a div to house the question
-    let feed_div = document.createElement('div');
-    feed_div.setAttribute('class', 'feed');
-    feed_div.setAttribute('id', post_id);
+    let question_div = set_challenge_question(question.uid);
 
-    // Create a div to get the image and name of the posting user
-    let user_info = document.createElement('div');
-    user_info.setAttribute('class', 'posting-user-info');
+    // Create a div to house the answer set and create answers within
+    let answer_set_div = set_challenge_answer_set(question.uid);
 
-    let user_img = document.createElement('img');
-    user_img.setAttribute('class', 'posting-user-img');
+    // Add a listener
+    option.addEventListener('click', function(){
+        let curr_num = this.getAttribute('id').slice(-1);
+        let curr_post_id = this.parentElement.parentElement.getAttribute('id');
+        for (let j = 0; j < 4; j++){
+            document.getElementById(curr_post_id +j).classList.remove('selected');
+            document.getElementById(curr_post_id +j).classList.add('not_selected');
+        }
+        option.classList.add('selected');
+        answers_ref.doc(post_id).set({answer: curr_num}, {merge: true});
+    })
 
-    let user_name = document.createElement('p');
-    user_name.setAttribute('class', 'posting-user-name');
 
-    user_info.appendChild(user_img);
-    user_info.appendChild(user_name);
 
-    get_posting_user_profile(user_img, user_name, question.uid);
-
-    feed_div.appendChild(user_info);
-
-    // Create a question element
-    let question_div = document.createElement('div');
+}
+function set_challenge_question(question){
+    question_div = document.createElement('div');
     question_div.setAttribute('class', 'question');
     question_div.innerHTML = question.question;
+    return question_div
+}
 
-    // Create answer div
-    let answer_div = document.createElement('div');
-    answer_div.setAttribute('class', 'answers');
+function set_challenge_answer_set(question){
+    let answer_set_div = document.createElement('div');
+    answer_set_div.setAttribute('class', 'answers');
+    for( let i = 0; i < question.answers.length; i++){
+        let answer_div = document.createElement('p');
+        if(post_answer == i){
+            answer_div.setAttribute('class', 'option selected');
 
-    // Create different options
-    for (let i = 0; i < question.answers.length; i++){
-        // Create an option
-
-
-        let option = document.createElement('p');
-        option.setAttribute('id', post_id + i);
-
-
-        if (post_answer == i){
-            option.setAttribute('class', 'option selected');
-        } else {
-
-            option.setAttribute('class', 'option not_selected');
+        } else{
+            answer_div.setAttribute('class', 'option not_selected');
         }
+        answer_div.innerHTML = question.answers[i];
+        answer_set_div.appendChild(answer_div);
+    }
+}
+function create_feed(question_obj, post_answer){
 
-        option.innerHTML = question.answers[i];
-        answer_div.appendChild(option);
 
 
         option.addEventListener('click', function(){
@@ -104,7 +121,7 @@ function create_feed(question_obj, post_answer){
             option.classList.add('selected');
             answers_ref.doc(post_id).set({answer: curr_num}, {merge: true});
         })
-    }
+
 
     feed_div.appendChild(question_div);
     feed_div.appendChild(answer_div);
@@ -165,13 +182,3 @@ submit_question.addEventListener('click', function(){
 
 });
 
-
-function get_posting_user_profile(user_img, user_name, uid){
-    users_ref.doc(uid).get()
-        .then(result => {
-            user_img.src = result.data().photoURL;
-            user_name.innerHTML = result.data().name;
-        }).catch(error => {
-            console.log(error.message);
-    })
-}
